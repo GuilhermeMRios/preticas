3763dd536e31dbaf21b77a0291f2a1c1b69ea811
GuilhermeMRios <gmonteirorios@gmail.com>
2024-03-05 10:09:17 -0300

    prova

---
+++ bahia/pratica/js/curso-connectar/logica/prova.js
@@ -0,0 +1,6 @@
+/*
+  Crie um algoritimo que forneça nome e sobrenome de cada um dos 10 alunos de uma escola.
+
+  O algoritimo tambme deve informar se o aluno foi aprovado (media das notas maior ou igual a 7) ou reprovado (media das notas menor que 7). */
+
+  
